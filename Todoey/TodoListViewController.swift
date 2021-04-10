ca5a4358faa1ee920b607e5d808ba6270eaac72a
Oshedhe <oshedhe91gmail.com>
2021-04-10 08:53:48 +0200

    Now you can add some task in the table. Next mission is to learn about AppDelegate like Android pause() resume() act

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    var itemArray = ["French Bulldog" , "Pug" , "English Bulldog"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        itemArray.count //creating 3 cells in table view
+    }
+    
+    //MARK: - Tableview Datasourse Methods
+    //cells of row at indexPath
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK: - Tableview Delegate Methods
+    //When user click on one cell...
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      //  print(itemArray[indexPath.row])
+        
+        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{//if it doesn't have any checkmarks
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    //MARK: - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        //when user pressed add button it should come up alert pop up message
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        //Button to Add Item in pop up message
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            print("Sucess!")
+            //when user has write on the textField and pressed Add
+            
+           // print(textField.text) //this will return Optional.. so we need to wrap!
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData() // to refresh the tabel otherwise you won't see your new lable
+        }
+        
+        //When user write in the text in pop up message
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+
+}
+
